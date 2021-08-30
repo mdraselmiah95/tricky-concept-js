20ba05ed91def6e1dd9092bdcf9b1f39fa46462a
rasel <russelroy15@gmail.com>
2021-08-30 12:28:18 +0600

    new condition concept

---
+++ double-triple.js
@@ -0,0 +1,93 @@
+/* const first = 2;
+const second = '2';
+if (first == second) {
+    console.log('condition is true');
+} else {
+    console.log('condition is False');
+} */
+
+
+
+/* const first = 2;
+const second = '2';
+
+if (first === second) { //=== it wil strictly compare the variable
+    console.log('condition is true');
+} else {
+    console.log('condition is False');
+} */
+
+
+/* const first = 1;
+const second = true;
+
+if (first == second) {
+    console.log('condition is true');
+} else {
+    console.log('condition is False');
+} */
+//condition is true
+
+/* const first = 1;
+const second = true;
+
+if (first === second) {
+    console.log('condition is true');
+} else {
+    console.log('condition is False');
+}
+ */
+//condition is False
+
+/* const first = '1';
+const second = true;
+
+if (first === second) {
+    console.log('condition is true');
+} else {
+    console.log('condition is False');
+}
+ */
+//condition is False
+
+/* const first = '0';
+const second = false;
+
+if (first == second) {
+    console.log('condition is true');
+} else {
+    console.log('condition is False');
+}
+ */
+//condition is true
+
+
+//more comparison
+
+/* const a = {
+    name: 'Rohim'
+};
+const b = {
+    name: 'Rohim'
+};
+
+if (a == b) {
+    console.log('Both are same');
+} else {
+    console.log('They are not same');
+}
+ */
+//They are not same
+
+
+
+const a = [];
+const b = [];
+
+if (a == b) {
+    console.log('Both are same');
+} else {
+    console.log('They are not same');
+}
+
+//They are not same
