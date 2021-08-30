c4121b5d67d21408a4a9f96f6a02ec7118826a6a
rasel <russelroy15@gmail.com>
2021-08-30 11:51:09 +0600

    variable concept

---
+++ true-false.js
@@ -0,0 +1,35 @@
+/* 
+Falsy: 
+false
+0
+empty string
+undefined
+null
+NaN
+-------------------
+Truthy:
+true
+any number (positive or negative )
+any string including single whitespace, '0', 'false'
+[]
+{}
+anything else that is not falsy will be truthy
+*/
+
+// const x = 11;
+// const x = -11;
+// const x = true;
+// const x = false;
+// const x = ' ';
+// const x = '';
+// let x = null;
+// let x = NaN;
+// let x = [];
+let x = {};
+
+
+if (x) {
+    console.log('variable is Truthy.')
+} else {
+    console.log('Variable is Falsy')
+}
